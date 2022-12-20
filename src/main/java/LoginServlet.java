e4f1852acbf0ab8160e4545e3b46324082d03d52
Marius.Birk <marius.birk@openknowledge.de>
2022-12-20 12:44:06 +0100

    Login/Logout with/out Cookies

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,66 @@
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private final String userID = "admin";
+    private final String password = "password";
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+        // get request parameters for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+
+        if(userID.equals(user) && password.equals(pwd)){
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "Pankaj");
+            //setting session to expiry in 30 mins
+            session.setMaxInactiveInterval(30*60);
+            Cookie userName = new Cookie("user", user);
+            response.addCookie(userName);
+            //Get the encoded URL string
+            String encodedURL = response.encodeRedirectURL("index.jsp");
+            response.sendRedirect(encodedURL);
+        }else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out= response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(request, response);
+        }
+
+    }
+
+    public ImageOutputStream createGrafic() throws IOException {
+        int width = 600, height = 600;
+        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D ig2 = bi.createGraphics();
+        ig2.fillRect(0, 0, width - 1, height - 1);
+        Iterator imageWriters = ImageIO.getImageWritersByFormatName("gif");
+        ImageWriter imageWriter = (ImageWriter)imageWriters.next();
+        File file = new File("filename.gif");
+        ImageOutputStream ios = ImageIO.createImageOutputStream(file);
+        imageWriter.setOutput(ios);
+        imageWriter.write(bi);
+        return ios;
+    }
+
+}
